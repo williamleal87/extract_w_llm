dba8555a1a7bb7af4e7660e5096be3f24e62290b
William Leal Barbosa <william.leal.87@gmail.com>
2025-08-23 17:01:20 -0300

    inserindo html_css.py

---
+++ html_css.py
@@ -0,0 +1,22 @@
+CSS_RODAPE = """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f1f1f1;
+        color: #666;
+        text-align: center;
+        padding: 10px;
+        font-size: 14px;
+        border-top: 1px solid #e1e1e1;
+    }
+    </style>
+"""
+
+HTML_RODAPE = """
+    <div class="footer">
+        <p>Desenvolvido com ❤️ por Will Leal | © 2025</p>
+    </div>
+"""
